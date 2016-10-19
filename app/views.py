0a8e467351ec821c2fe726de4b8088bf9a323210
Jobling <tiago.jobling@gmail.com>
2016-10-19 14:43:52 +0100

    Version 0.2
    Started using templates and for loops in html through Jinja2 templating
    engine.
    Later, the post mechanics can be applied to a list of mac addresses and
    networks.

--- app/views.py
+++ app/views.py
@@ -1,16 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
 	user = {'nickname': 'Jobling'} # fake user
-	return '''
-<html>
-	<head>
-		<title>Home Page</title>
-	</head>
-	<body>
-		<h1>Hello, ''' + user['nickname'] + '''</h1>
-	</body>
-</html>
-'''
+	posts = [ # fake array of posts
+		{
+			'author': {'nickname': 'Tiago'},
+			'body': 'Está um lindo dia para comer bolos!'
+		},
+		{
+			'author': {'nickname': 'Rita'},
+			'body': 'Podemos ir passear!'
+		}
+	]
+	return render_template('index.html',
+							title = 'Home',
+							user = user,
+							posts = posts)
