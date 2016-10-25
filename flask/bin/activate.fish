d72719b6db19d54268ba0add786c375f54431dc4
Jobling <tiago.jobling@gmail.com>
2016-10-25 10:14:04 +0100

    Configuring vm

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jobling/web-python/flask"
+set -gx VIRTUAL_ENV "/home/jobling/Flask_WebApp/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
